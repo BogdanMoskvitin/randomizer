34977542cc93e813bdbf2ed4549586e3595ec0ed
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2021-06-14 11:51:16 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'randomizer';
+
+  names = [];
+
+  add(name){
+    this.names.push(name);
+  }
+
+  lastNumber = 0;
+  randomName;
+
+  getRandom() {
+    let min = 1;
+    let max = this.names.length;
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (randomNumber != this.lastNumber) {
+      this.randomName = this.names[randomNumber - 1];
+      this.lastNumber = randomNumber;
+    } else {
+      this.getRandom();
+    }
+  }
 }
